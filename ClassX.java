6985de326093c655d2d6d41c3258c0d6b1e9525f
KJRMalshan <ravindumalshan1111@gmail.com>
2024-12-17 14:16:27 +0530

    New try

---
+++ ClassX.java
@@ -0,0 +1,9 @@
+public class ClassX {
+    public void methodX() {
+        System.out.println("No constructor defined . MethodX Writes Hello!");
+    }
+public static void main(String args[]){
+        ClassX obj = new ClassX();
+        obj.methodX();
+}
+}
